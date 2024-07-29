fb2868ff66c337b2d70e96ba362e129bafc32804
GAMING <GAMING@DESKTOP-3EUSUM2>
2024-07-29 09:50:59 +0100

    Add project files.

---
+++ problem solving 16/problem solving 16.cpp
@@ -0,0 +1,27 @@
+// problem solving 16.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a, d;
+    
+    cout << "please enter a " << endl;
+    cin >> a;
+    cout << "please enter d " << endl;
+    cin >> d;
+
+
+    cout << "Area = " << a * sqrt(pow(d, 2) - pow(a, 2)) << endl;
+
+
+
+
+
+
+
+    return 0;
+}
+
